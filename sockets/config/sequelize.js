10e8fb1be138e7a67f47c21255929311b9125358
BimalKanxa <sharmabimal2003@gmail.com>
2025-07-08 05:35:41 +0530

    postgres issue with env

--- sockets/config/sequelize.js
+++ sockets/config/sequelize.js
@@ -1,9 +1,25 @@
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+//   logging: false
+// });
+
+// module.exports = sequelize;
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  logging: false
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // ✅ Important for Render self-signed certs
+    }
+  }
 });
 
 module.exports = sequelize;
